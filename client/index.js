4b52540cc9667538727090c2620c01a0cb002c55
Dennis Ent <dennisent@Denniss-MacBook-Pro.local>
2019-07-27 14:41:34 +1000

    d

--- client/index.js
+++ client/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+// import ApolloClient from 'apollo-client';
+// import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({});
+// const client = new ApolloClient({});
 
 const Root = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>Lyrical</div>
-    </ApolloProvider>
-  );
+  return <div>dennis</div>;
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
